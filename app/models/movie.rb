59eb7f48dbea7560cbee477609b72a43778213f4
Chris <ckirkinis@gmail.com>
2015-06-09 11:23:50 -0400

    added notes for next time

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+
+	# NEXT STEPS
+		# Import movie lists for Netflix and HBO Go to db
+		# Create ratings model
+		# Look up all ratings for each movie, save to db
+		
 end
